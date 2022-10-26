3d9e5c7d6007e89bbba2e586da3c19db144a6f1b
Igor2020888 <80600039+Igor2020888@users.noreply.github.com>
2022-10-26 20:42:08 +0700

    Add files via upload

---
+++ roles/deploy_openvpn/files/skript2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat user1_for_generate.conf \
+    <(echo -e '\n\n<ca>') ca.crt \
+    <(echo -e '</ca>\n\n\n<cert>') \
+    user1.crt <(echo -e '</cert>\n\n\n<key>') \
+    user1.key <(echo -e '</key>\n\n\n<tls-auth>') \
+    ta.key <(echo -e '</tls-auth>\n\n\n ') \
+    user1_for_generate2.conf <(echo -e ' ') \
+    > user1_main.conf
+
+
